930d23a8bf3aa47315d6465c43a91dc945fd2e33
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-08-04 16:25:37 +0300

    pre release

--- Assets/JS/custom.js
+++ Assets/JS/custom.js
@@ -1,6 +1,6 @@
 /*jshint esnext: true */
 /* navigation */
-$("#toggler, #overlay").click(()=>{
+$("#toggler, #overlay, .link").click(()=>{
   $("#menu").toggleClass("max-lg:-translate-y-full")
   $("#overlay").toggleClass("hidden")
   $("#toggler").toggleClass("h-5")
@@ -9,3 +9,40 @@ $("#toggler, #overlay").click(()=>{
   $("#toggler > :nth-child(3)").toggleClass("-rotate-45")
   $("body").toggleClass("overflow-hidden")
 })
+
+/* sliders */
+const swiper = new Swiper('.examples-1', {
+  loop: true,
+  navigation: {
+    nextEl: '.examples-1-next',
+    prevEl: '.examples-1-prev',
+  },
+});
+const swiper2 = new Swiper('.examples-2', {
+  loop: true,
+  navigation: {
+    nextEl: '.examples-2-next',
+    prevEl: '.examples-2-prev',
+  },
+});
+const swiper3 = new Swiper('.examples-3', {
+  loop: true,
+  navigation: {
+    nextEl: '.examples-3-next',
+    prevEl: '.examples-3-prev',
+  },
+});
+const swiper4 = new Swiper('.examples-4', {
+  loop: true,
+  navigation: {
+    nextEl: '.examples-4-next',
+    prevEl: '.examples-4-prev',
+  },
+});
+const swiper5 = new Swiper('.examples-5', {
+  loop: true,
+  navigation: {
+    nextEl: '.examples-5-next',
+    prevEl: '.examples-5-prev',
+  },
+});
